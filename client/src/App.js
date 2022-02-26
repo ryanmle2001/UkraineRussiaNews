d6ff8a837aec1f0cc9a957a5d7e33fab0af48a59
Justin <justinparkcs@gmail.com>
2022-02-26 00:15:26 -0500

    first commit for frontend

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
