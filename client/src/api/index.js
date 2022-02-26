94ada8409062850f8cb9dc4611472a0fb0e9562f
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2022-02-26 12:17:37 -0500

    Update api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,21 +1,21 @@
 var axios = require("axios").default;
 
-export const getData = async () => {
-    var options = {
-      params: {
+export const getData = async ({cities}) => {
+    // var options = {
+    //   params: {
 
-      },
-      headers: {
-        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-        "x-rapidapi-key": "b51d086105msh50ca0aa73611a01p10c4b8jsn910a1ef00f1f",
-      },
-    };
-  
+    //   },
+    //   headers: {
+    //     "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+    //     "x-rapidapi-key": "b51d086105msh50ca0aa73611a01p10c4b8jsn910a1ef00f1f",
+    //   },
+    // };
+    
     try {
       const {
         data: { data },
       } = await axios.get(
-        `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`
+        `https://localhost`
       );
       return data;
     } catch (error) {
