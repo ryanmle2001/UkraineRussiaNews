1dbec206de12d6961a43ea6277ccd2bf0791cc3c
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2022-02-26 19:10:43 -0500

    Update everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,53 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar/Navbar";
 import Search from "./pages/Search/Search";
-import {Box} from '@material-ui/core';
-
+import { Box } from "@material-ui/core";
+import Map from "./components/Map/Map";
+import ClockLoader from "react-spinners/ClockLoader";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+    return () => {};
+  }, []);
+
   return (
     <>
-      <Navbar></Navbar>
-      <br />
-      <Box sx={{ mx: "auto", width: 200 }} >
-        <Search></Search>
-      </Box>
-      <br />
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      {loading ? (
+        <Box sx={{position: 'fixed', top:'50%', left: '50%'}}>
+          <ClockLoader
+            className="spinner"
+            size={150}
+            color={"black"}
+            loading={loading}
+          />
+        </Box>
+      ) : (
+        <Box>
+          <Navbar></Navbar>
+          <br />
+          <Box sx={{ mx: "auto", width: 200 }}>
+            <Search></Search>
+          </Box>
+          <br />
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+            </Routes>
+            <Routes>
+              <Route exact path="/map" element={<Map />} />
+            </Routes>
+          </Router>
+        </Box>
+      )}
     </>
   );
 }
--- client/src/pages/Navbar/Navbar.js
+++ client/src/pages/Navbar/Navbar.js
@@ -1,27 +1,27 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState } from "react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import {AiOutlineMenu} from 'react-icons/ai'; 
-import {Spacer} from '@material-ui/core'; 
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import FontLink from '../../components/FontLink';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Spacer } from "@material-ui/core";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import FontLink from "../../components/FontLink";
+import { Link } from "react-router-dom";
 
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-const options = ['Option 1', 'Option 2'];
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const options = ["Option 1", "Option 2"];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -43,61 +43,46 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar color="transparent" position="static" sx={{py: 2}}>
+    <AppBar color="transparent" position="static" sx={{ py: 2 }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters color="transparent">
-          <Typography
-            variant="h3"
-            noWrap
-            color="black"
-            sx={{ mx: 'auto'}}
-          >
-           Ukraine
+          <Typography variant="h3" noWrap color="black" sx={{ mx: "auto" }}>
+            Ukraine
           </Typography>
 
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-            className="font-link"
-          >
-            LOGO
-          </Typography>
-           
-           <br />
+          <br />
 
-
-
-          <Box sx={{ flexGrow: 0 }} >
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <AiOutlineMenu />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+  
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <AiOutlineMenu />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">
+                    Map
+                  </Typography>
                 </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              </Menu>
+            </Box>
+         
         </Toolbar>
       </Container>
     </AppBar>
