83c277d03912f365c51208b44e968f159c72d36a
ryanmle2001 <ryanmle2001@gmail.com>
2022-02-27 00:12:49 -0500

    Ryan branch

--- server/app.py
+++ server/app.py
@@ -1,19 +1,18 @@
 from flask import Flask, url_for, render_template, request, redirect, abort, jsonify, session, flash, make_response
+# from flask_cors import CORS, cross_origin
 import requests
 from pip._vendor import cachecontrol
 import models as db
 
 
-app = Flask(__name__, )
-app.secret_key="123"
-
+app = Flask(__name__)
 cities = set()
 with open("ua_cities.json") as file:
     data = file.readlines()
     for i in data:
         cities.add(i.strip())
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     news = db.get_national_news()
     city_info = []
@@ -26,9 +25,11 @@ def index():
             "news": news
         }
     }
-    return jsonify(data)
+    response = jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-@app.route('/<string:city>')
+@app.route('/<string:city>', methods=['GET'])
 def load_city(city):
     if city.lower() not in cities:
         return redirect("/")
@@ -40,7 +41,10 @@ def load_city(city):
             "news": news
         }
     }
-    return jsonify(data)
+    response = jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
